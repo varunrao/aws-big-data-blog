aacc520c75b6ee8060a7d48608f933cc8320c972
vbhamidi <vbhamidi@amazon.com>
2017-01-10 23:19:57 -0600

    Add ignore Quote, was breaking Hue higher versions

--- aws-blog-emr-ranger/scripts/emr-steps/updateHueLdapUrl.sh
+++ aws-blog-emr-ranger/scripts/emr-steps/updateHueLdapUrl.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 set -x
 ldap_ip_address=$1
-sudo sed -i "s|ldap://.*|ldap://$ldap_ip_address|g" /etc/puppet/hieradata/site.yaml
+sudo sed -i "s|ldap://.*[^']|ldap://$ldap_ip_address|g" /etc/puppet/hieradata/site.yaml
 cd  /var/aws/emr/
 sudo puppet apply --verbose -d --modulepath="bigtop-deploy/puppet/modules:/etc/puppet/modules" bigtop-deploy/puppet/manifests/site.pp -e 'include hue::server' 2>&1 | tee ~/puppet_apply.log
 sudo puppet apply -e 'service { "hue": ensure => false, }'
